18f4b84f90cbefbd1fb9c5360c0a6648f9033c8d
xiaoqing <2397328520@qq.com>
2018-05-01 23:06:11 +0800

    checkArr

---
+++ init/checkArr.js
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>checkArr</title>
+	<style type="text/css">
+		*{
+			margin:0;
+			padding:0;
+		}
+		.box{
+			width: 100px;
+			height:115px;
+			border:1px solid #ccc;
+			margin:50px auto 0;
+			position: relative;
+		}
+		p{
+			width: 100px;
+			height: 20px;
+			position: absolute;
+			left:0px;
+			background: rgba(0,0,0,.6);
+			font-size: 10px;
+			line-height: 20px;
+			text-align: center;
+		}
+		#upText{
+			top:0;
+		}
+		#downText{
+			bottom:0;
+		}
+		img{
+			margin-top: 20px;
+		}
+		a{
+			width: 20px;
+			height: 20px;
+			position: absolute;
+			top:52px;
+			text-decoration: none;
+			color: #fff;
+			font-size: 14px;
+			background: rgba(0,0,0,.6);
+			text-align: center;
+			opacity: 0.6;
+		}
+		a:hover{
+			opacity: 0.9;
+		}
+		#pre{
+			left:0px;
+		}
+		#next{
+			right:0px;
+		}
+	</style>
+	<script type="text/javascript">
+		window.onload = function(){
+			var upText = document.getElementById('upText');
+			var downText = document.getElementById('downText');
+			var imgCont = document.getElementById('imgCont');
+			var pre = document.getElementById('pre');
+			var next = document.getElementById('next');
+
+			var arrUrl = ['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg'];
+			var arrInfo = ['图片1','图片2','图片3','图片4'];
+
+			var num = 0;
+			function init(){
+				imgCont.src = arrUrl[num];
+				upText.innerHTML = num + 1 +'/' + arrUrl.length;
+				downText.innerHTML = arrInfo[num];
+			}
+			init();
+
+			next.onclick = function(){
+				num ++;
+				if(num == arrUrl.length){
+					num = 0;
+				}
+				init();
+			}
+
+			pre.onclick = function(){
+				num --;
+				if(num < 1){
+					num = arrUrl.length - 1;
+				}
+				init();
+			}
+		}
+	</script>
+</head>
+<body>
+<div class="box" id="box">
+	<p id="upText">正在加载……</p>
+	<p id="downText">正在加载……</p>
+	<img id="imgCont" src=""/>
+	<a id="pre" href="javascript:;">&lt;</a>
+	<a id="next" href="javascript:;">&gt;</a>
+</div>
+</body>
+</html>
