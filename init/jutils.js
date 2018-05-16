ed32dfc2e528972b66d315e4a89f1cf283aacb61
xiaoqing <2397328520@qq.com>
2018-05-16 15:20:45 +0800

    test go on

---
+++ init/jutils.js
@@ -0,0 +1,43 @@
+//获得页面html标签的属性值
+function getStyle(obj,attr){
+	return obj.currentStyle ? obj.currentStyle[attr] : getComputedStyle(obj)[attr];
+}
+
+//小小运动
+function doMove(obj,dir,target,attr,fn){
+	dir = parseInt(getStyle(obj,attr)) < target ? dir : -dir;
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		speed = parseInt(getStyle(obj,attr)) + dir;
+		if( speed > target && dir > 0 ||  speed < target && dir < 0 ){
+			speed = target;
+		}
+		obj.style[attr] = speed + 'px';
+		if( speed == target ){
+			clearInterval(obj.timer);
+		}
+		fn && fn();
+	},30);
+}
+
+//抖函数
+function shake(obj,attr,fn){
+	var num = 0;
+	var arr = [];
+	var timer = null;
+	var pos = parseInt(getStyle(obj,attr));
+	for(var i = 20; i > 0; i -= 2){
+		arr.push(i,-i);
+	}
+	arr.push(0);
+
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		obj.style[attr] = pos + arr[num] + 'px';
+		num ++;
+		if( num == arr.length){
+			clearInterval(obj.timer);
+			fn && fn();
+		}
+	},50);
+}
